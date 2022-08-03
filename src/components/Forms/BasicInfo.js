cc386c1eceb9c92cb64c13c9228be9958560ed8a
Tsion18 <sisaytsion17@gmail.com>
2022-08-03 12:06:29 +0300

    updated merge

--- src/components/Forms/BasicInfo.js
+++ src/components/Forms/BasicInfo.js
@@ -1,20 +1,44 @@
 import * as React from "react";
 import { Box, TextField } from "@mui/material";
+import { CV } from "../MainCard";
+
 
 export default function BasicInfo() {
+  const CVContext = React.useContext(CV)
+  const setUser = CVContext.setUser;
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { mr: 10, ml: 5, mb: 5.5, mt: 0.5, width: '35ch' },
+        "& .MuiTextField-root": {
+          mr: 10,
+          ml: 5,
+          mb: 5.5,
+          mt: 0.5,
+          width: "35ch",
+        },
       }}
       noValidate
       autoComplete="off"
     >
-    <div>
-    <TextField id="standard-basic" label="Highschool" variant="standard" />
-    <TextField id="standard-basic" label="College/Community School" variant="standard"/>
-</div>
+      <div>
+      {CVContext.user.basicInfo.name}
+        <TextField id="standard-basic"
+        onChange={(ev)=>{
+          setUser(
+            
+          )
+        }}
+        
+        label="Highschool" variant="standard" />
+        <TextField
+          id="standard-basic"
+          label="College/Community School"
+          variant="standard"
+        />
+      </div>
     </Box>
-  )
+  );
 }
+
+
