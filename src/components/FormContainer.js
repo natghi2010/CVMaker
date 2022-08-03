ed2657d8c548740ec31a3588c3541e3cb491c9fd
Tsion18 <sisaytsion17@gmail.com>
2022-08-03 11:12:12 +0300

    basic form

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,14 +1,16 @@
 import { useContext } from "react";
+import BasicInfo from "./Forms/BasicInfo";
 import { CV } from "./MainCard";
 
 function FormContainer() {
 
   const CVContext = useContext(CV);
 
+  console.log(CVContext);
     return (
       <div className="formContainer">
-         <h2>{CVContext.steps[CVContext.activeStep].label}</h2>
-         
+          <h2>{CVContext.steps[CVContext.activeStep].label}</h2>
+          
       </div>
     );
   }
--- src/components/Forms/BasicInfo.js
+++ src/components/Forms/BasicInfo.js
@@ -1,1 +1,27 @@
+import * as React from "react";
+import { Box, TextField } from "@mui/material";
 
+export default function BasicInfo() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { mr: 10, ml: 5, mb: 5.5, mt: 0.5, width: '35ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField id="standard-basic" label="First Name" type="Standard"/>
+        <TextField id="standard-basic" label="Last Name" type="Standard"/>
+      </div>
+      <div>
+        <TextField id="standard-number" label="Age" type="number"/>
+        <TextField id="standard-basic" label="Email Address" type="Standard"/>
+      </div>
+      <div>
+        <TextField id="standard-number" label="Phone Number" type="number"/>
+      </div>
+    </Box>
+  )
+}
