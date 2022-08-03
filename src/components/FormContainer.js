b987fcac3af40a5e2a840c9930e0aebddbfac6b4
Daniel Bekele <bekele01@allegheny.edu>
2022-08-03 11:10:11 +0300

    creating different forms.

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,14 +1,17 @@
-import { useContext } from "react";
+import { Component, useContext } from "react";
+import Education from "./Forms/Education";
 import { CV } from "./MainCard";
+
 
 function FormContainer() {
 
   const CVContext = useContext(CV);
 
+  console.log(CVContext);
     return (
       <div className="formContainer">
          <h2>{CVContext.steps[CVContext.activeStep].label}</h2>
-         
+         <Education/>
       </div>
     );
   }
