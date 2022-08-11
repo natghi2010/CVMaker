a9ab5b4626700fd01315f18c0a91ff296f8a4ffb
Natnael Ghirma <natnael.g@suntecholding.com>
2022-08-11 11:51:53 +0300

    updated

--- src/components/CVPDF.js
+++ src/components/CVPDF.js
@@ -37,32 +37,72 @@ export default function CVPDF() {
         </Grid>
 
         <Grid item xs={6}>
-          Date of Birth .....................................04/04/2022
+        Nationality .....................................Ethiopia
         </Grid>
 
         <Grid item xs={6}>
-          Place of Birth ..............................Addis Ababa
+          Gender ............................................ &nbsp;Male
         </Grid>
       </Grid>
 
       <div className="section-container">
         <h3 className="section-title">Profile</h3>
       </div>
+      <div>
+        <div className="section-container">
+          <h3 className="section-title">Education</h3>
+        </div>
 
-      <div className="contact-container"></div>
+        <div className="expereince-container">
+          <span>
+            <span className="job-title">Insitution</span>
+
+            <span className="job-duration"> &nbsp;Oct 2015 - Jan 2017</span>
+          </span>
+        <span className="type-of-degree"> 
+         <br/> 
+         <i>Bacholors</i>
+        </span>
+        
+        </div>
+        </div>
+
+  <br/><br/>
+
       <div>
         <div className="section-container">
           <h3 className="section-title">Experience</h3>
         </div>
-          <p>
-            testing pdf . testing pdf. testing pdf. testing pdf . testing pdf .
-            testing pdf. testing pdf. testing pdf . testing pdf. testing pdf.
-            testing pdf . testing pdf. testing pdf. testing pdf . testing pdf.
-            testing pdf. testing pdf . testing pdf. testing pdf. testing pdf .
-            testing pdf. testing pdf. testing pdf . testing pdf. testing pdf.
-            testing pdf . testing pdf. testing pdf.
-          </p>
-      </div>
+
+        <div className="expereince-container">
+          <span>
+            <span className="job-title">Job Title, Company</span>
+
+            <span className="job-duration"> &nbsp;Oct 2015 - Jan 2017</span>
+          </span>
+
+          <div className="job-description">
+            <p>
+              Id exercitation labore eiusmod laboris enim laborum ipsum
+              adipisicing irure aute ad eiusmod nostrud. Sit culpa proident
+              aliquip magna aliqua consectetur deserunt laboris pariatur.
+              Nostrud officia esse adipisicing irure nostrud Lorem anim dolor.
+            </p>
+          </div>
+          <hr />
+        </div>
+        </div>
+
+        <div className="section-container">
+          <h3 className="section-title">Skills</h3>
+        </div>
+
+        <div className="skills-container">
+          <div className="skill-item">
+            <span className="skill-name">Skill Name</span>
+          </div>
+        </div>
+   
     </div>
   );
 }
