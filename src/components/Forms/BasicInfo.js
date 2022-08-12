5c2f63b8b5e842361989a766d73b7a40bfdfc671
Natnael Ghirma <natnael.g@suntecholding.com>
2022-08-12 11:11:56 +0300

    Updated Basic Info

--- src/components/Forms/BasicInfo.js
+++ src/components/Forms/BasicInfo.js
@@ -18,26 +18,11 @@ export default function BasicInfo() {
   const CVContext = React.useContext(CV);
   const setUser = CVContext.setUser;
   const user = CVContext.user;
-  const [loading,setLoading] = useState(false);
-  const [listOfContries, setListOfCountries] = useState([]);
 
-  const getNationalities = async () => {
-    setLoading(true);
-    const response = await http.post(
-      "https://countriesnow.space/api/v0.1/countries/positions/range",
-      {
-        type: "long",
-        min: 1,
-        max: 40,
-      }
-    );
-    setListOfCountries(response.data);
-    setLoading(false);
-  };
+  const [countries,setCountries] = useState([]);
 
-  useEffect(() => {
-    getNationalities();
-  },[]);
+  
+ 
 
   return (
     <Box
@@ -129,15 +114,35 @@ export default function BasicInfo() {
           />
         </Grid>
         <Grid item xs={6}>
-          {!loading && <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={listOfContries}
-            getOptionLabel={(option) => option.name}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Nationality" />}
-          />}
-          {loading && <Typography center>Loading..</Typography>}
+        <Autocomplete
+      id="country-select-demo"
+      sx={{ width: 300 }}
+      options={listOfContries}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      renderOption={(props, option) => (
+        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+          <img
+            loading="lazy"
+            width="20"
+            src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+            srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+            alt=""
+          />
+          {option.label} ({option.code}) +{option.phone}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose a country"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
         </Grid>
         <Grid item xs={6}>
           <TextField
