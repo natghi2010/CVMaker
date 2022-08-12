dac14ee48dfcf4f3dcd08eeafcd8792f0b7744c8
Natnael Ghirma <natnael.g@suntecholding.com>
2022-08-12 17:09:47 +0300

    Updated skills

--- src/components/Forms/Skills.js
+++ src/components/Forms/Skills.js
@@ -58,7 +58,7 @@ export default function Skills() {
           );
         })}
       </Grid>
-      <Button variant= "contained" onClick={()=>{CVContext.handlePrint()}}> Print</Button>
+   
     </Stack>
   );
 }
