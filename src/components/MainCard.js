d7cdba0dc1abec91fa4e957bc2f1e569f5b1c1a8
Daniel Bekele <bekele01@allegheny.edu>
2022-08-15 11:17:46 +0300

    adding if's for validations

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,58 +1,54 @@
-import React, { createContext, useRef,useEffect } from "react";
+import React, { createContext, useRef, useEffect } from "react";
 import FormContainer from "./FormContainer";
 import Sidenav from "./Sidenav";
 import Title from "./Title";
 import CVPDF from "./CVPDF";
 import Footer from "./Footer";
 import educationTypes from "./Forms/reference/educationTypeRef";
-import ReactToPrint, { useReactToPrint } from 'react-to-print';
+import ReactToPrint, { useReactToPrint } from "react-to-print";
 import { Button } from "@mui/material";
 
 export const CV = createContext();
 
 function MainCard() {
-  
   const [activeStep, setActiveStep] = React.useState(2);
 
- 
-  var userModel = window.localStorage.getItem("user")!= null ? JSON.parse(window.localStorage.getItem("user")) : {
-    basicInfo: {
-      name: "John Doe",
-      date_of_birth: "22/05/1995",
-      place_of_birth: "Addis Ababa",
-      gender:"Male",
-      nationality: "Ethiopian",
-      address: "Bole,Addis Ababa",
-      role:"Engineer",
-      email: "engineer@gmail.com",
-      phone_number: "+251947001077",
-    },
-    educationExperience:[
-      {
-        institution_type: 'High School',
-        instituion_name : 'Lancaster University',
-        graduation_year : '2000',
-        degree_type : 'Diploma',
-      },
-      
-    ],
-  
-    workExperinces: [
-      {
-        company_name: "",
-        title: "",
-        description: "",
-        start_date: "",
-        end_date: "",
-      },
-    ],
-    skills:[]
-    
-  };
+  var userModel =
+    window.localStorage.getItem("user") != null
+      ? JSON.parse(window.localStorage.getItem("user"))
+      : {
+          basicInfo: {
+            name: "John Doe",
+            date_of_birth: "22/05/1995",
+            place_of_birth: "Addis Ababa",
+            gender: "Male",
+            nationality: "Ethiopian",
+            address: "Bole,Addis Ababa",
+            role: "Engineer",
+            email: "engineer@gmail.com",
+            phone_number: "+251947001077",
+          },
+          educationExperience: [
+            {
+              institution_type: "High School",
+              instituion_name: "Lancaster University",
+              graduation_year: "2000",
+              degree_type: "Diploma",
+            },
+          ],
 
+          workExperinces: [
+            {
+              company_name: "",
+              title: "",
+              description: "",
+              start_date: "",
+              end_date: "",
+            },
+          ],
+          skills: [],
+        };
 
-
- 
   const [user, setUser] = React.useState(userModel);
 
   const addEducation = () => {
@@ -67,29 +63,27 @@ function MainCard() {
           degree_type: "",
         },
       ],
-    })
-  }
+    });
+  };
 
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
 
-   useEffect(() => {
+  useEffect(() => {
     window.localStorage.clear();
     window.localStorage.setItem("user", JSON.stringify(user));
-  },[user]);
-
-
-
+  }, [user]);
 
   const removeEducation = (index) => {
     setUser({
       ...user,
-      educationExperience: user.educationExperience.filter((_, i) => i !== index),
+      educationExperience: user.educationExperience.filter(
+        (_, i) => i !== index
+      ),
     });
-
-  }
+  };
 
   const addWorkExperience = () => {
     setUser({
@@ -105,22 +99,64 @@ function MainCard() {
         },
       ],
     });
-  }
+  };
 
-    const removeWorkExperience = (index) => {
-      setUser({
-        ...user,
-        workExperinces: user.workExperinces.filter((_, i) => i !== index),
-      });
-    }
-    
+  const removeWorkExperience = (index) => {
+    setUser({
+      ...user,
+      workExperinces: user.workExperinces.filter((_, i) => i !== index),
+    });
+  };
 
   const handleNext = () => {
-    if(activeStep === steps.length - 1){
+    if (activeStep === steps.length - 1) {
       handlePrint();
       return;
     }
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const validateBasicInfo = (basicInfo) => {
+      if (basicInfo.name.length == 0) {
+        alert("Insert name");
+        return false;
+      }
+
+      if (basicInfo.date_of_birth.length == 0) {
+        alert("Insert Date of birth");
+        return false;
+      }
+      if (basicInfo.place_of_birth.length == 0) {
+        alert("Insert Place of birth");
+        return false;
+      }
+
+      if (basicInfo.gender.length == 0) {
+        alert("Insert Gender");
+        return false;
+      }
+      if (basicInfo.nationality.length == 0) {
+        alert("Insert Nationality");
+        return false;
+      }
+      if (basicInfo.address.length == 0) {
+        alert("Insert Address");
+        return false;
+      }
+      if (basicInfo.role.length == 0) {
+        alert("Insert Role");
+        return false;
+      }
+      if (basicInfo.email.length == 0) {
+        alert("Insert Email");
+        return false;
+      }
+      if (basicInfo.phone_number.length == 0) {
+        alert("Insert Phone number");
+        return false;
+      }
+    };
+
+    if (validateBasicInfo) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
   };
 
   const handleBack = () => {
@@ -153,7 +189,6 @@ function MainCard() {
   return (
     <div className="card">
       <Title
-     
         title="CVMaker"
         subtitle="Follow the simple 4 Steps to complete your CV"
       />
@@ -173,6 +208,7 @@ function MainCard() {
           addWorkExperience,
           removeWorkExperience,
           handlePrint,
+          validateBasicInfo
         }}
       >
         <div className="cardBody">
@@ -180,11 +216,7 @@ function MainCard() {
           <FormContainer />
         </div>
 
-       
-        <div ref={componentRef}>
-          {<CVPDF />}
-        </div>
-
+        <div ref={componentRef}>{<CVPDF />}</div>
       </CV.Provider>
     </div>
   );
